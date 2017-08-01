7efd0a432ef758a0584b14e1f53b797795a1d80a
Brandon Stewart <boveus@gmail.com>
2017-08-01 09:06:41 -0600

    Add first test.

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,34 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
 
 end
+
+
+### Iteration 1: Pantry Stocking
+# Build a simple Pantry-tracking program that can store a list of ingredients and available
+# quantities. Once we have tracked our ingredients and quantities, we'll use the Recipe class
+# we built before to have the pantry check to see what we can make.
+# Support the following interactions:
+# ```ruby
+# pantry = Pantry.new
+# # => <Pantry...>
+# # Checking and adding stock
+# pantry.stock
+# # => {}
+# pantry.stock_check("Cheese")
+# # => 0
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
+# ```
